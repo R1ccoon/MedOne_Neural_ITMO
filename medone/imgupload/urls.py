67a6c143e82d776a81f38750688c81ca9131ca95
Siemens <sfoch@yandex.ru>
2023-11-20 16:08:24 +0300

    Fast fix display pictures

--- medone/imgupload/urls.py
+++ medone/imgupload/urls.py
@@ -1,13 +1,6 @@
 from django.urls import path
-from django.conf import settings
-from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
-    path("",views.image_upload_view),
+    path("", views.image_upload_view),
 ]
-
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
--- medone/imgupload/views.py
+++ medone/imgupload/views.py
@@ -5,12 +5,14 @@ from .forms import ImageForm
 def image_upload_view(request):
     """Process images uploaded by users"""
     if request.method == 'POST':
+        print(request.FILES)
+        print(request.POST)
         form = ImageForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
             # Get the current instance object to display in the template
             img_obj = form.instance
-
+            print(img_obj, 'это имаге')
             return render(request, 'index.html', {'form': form, 'img_obj': img_obj})
     else:
         form = ImageForm()
