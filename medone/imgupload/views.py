4bf420ff838ec6ae4e9bb0456ae3bbb7f595206b
Siemens <sfoch@yandex.ru>
2023-11-21 23:32:18 +0300

    Added jpg to png conversion

--- medone/imgupload/views.py
+++ medone/imgupload/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .forms import ImageForm
+from PIL import Image
 
 
 def image_upload_view(request):
@@ -10,9 +11,12 @@ def image_upload_view(request):
         form = ImageForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
+
             # Get the current instance object to display in the template
             img_obj = form.instance
-            print(img_obj, 'это имаге')
+            im1 = Image.open(fr"{img_obj.image.url[1:]}")
+            im1.save(fr'media/images/{img_obj.title}.png')
+
             return render(request, 'index.html', {'form': form, 'img_obj': img_obj})
     else:
         form = ImageForm()
